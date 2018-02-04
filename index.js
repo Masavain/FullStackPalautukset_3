f20aa482c7bd953b73628e0681abd18c6f9aa421
Masavain <matti.vainionpaa@helsinki.fi>
2018-02-04 17:07:41 +0200

    heroku initialize

--- index.js
+++ index.js
@@ -8,93 +8,93 @@ app.use(bodyParser.json())
 app.use(express.static('build'))
 
 
-morgan.token('data', function (req, res) { 
-    return (JSON.stringify({"name": req.body.name, "number": req.body.number}))
+morgan.token('data', function (req, res) {
+  return (JSON.stringify({ "name": req.body.name, "number": req.body.number }))
 })
 
 app.use(morgan(':method :url :data :status :res[content-length] - :response-time ms'))
 
 const generateId = () => {
-    const maxId = persons.length > 0 ? persons.map(p => p.id).sort().reverse()[0] : 1
-    return maxId + 1
-  }
+  const maxId = persons.length > 0 ? persons.map(p => p.id).sort().reverse()[0] : 1
+  return maxId + 1
+}
 
 
 let persons = [
-    {
-      name: 'Arto Hellas',
-      number: '040-123456',
-      id: 1
-    },
-    {
-      name: 'Martti Tienari',
-      number: '040-123456',
-      id: 2
-    },
-    {
-      name: 'Arto Järvinen',
-      number: '040-123456',
-      id: 3
-    },
-    {
-      name: 'Lea Kutvonen',
-      number: '040-123456',
-      id: 4
-    }
-  ]
+  {
+    name: 'Arto Hellas',
+    number: '040-123456',
+    id: 1
+  },
+  {
+    name: 'Martti Tienari',
+    number: '040-123456',
+    id: 2
+  },
+  {
+    name: 'Arto Järvinen',
+    number: '040-123456',
+    id: 3
+  },
+  {
+    name: 'Lea Kutvonen',
+    number: '040-123456',
+    id: 4
+  }
+]
 
-  app.get('/api/persons', (req, res) => {
-    res.json(persons)
-  })
+app.get('/api/persons', (req, res) => {
+  res.json(persons)
+})
 
-  app.get('/info', (req, res) => {
-    const date = new Date()
+app.get('/info', (req, res) => {
+  const date = new Date()
 
-    res.send(`<p>Puhelinluettelossa on ${persons.length} henkilön tiedot</p>
+  res.send(`<p>Puhelinluettelossa on ${persons.length} henkilön tiedot</p>
     <p>${date}</p>`)
-  })
+})
 
-  app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(p => p.id === id )
+app.get('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  const person = persons.find(p => p.id === id)
 
-    if ( person ) {
-      response.json(person)
-    } else {
-      response.status(404).end()
-    }
-  })
-
-  app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter(p => p.id !== id)
-  
-    response.status(204).end()
-  })
-
-  app.post('/api/persons/', (request, response) => {
-    const body = request.body
-
-    if (body.name === undefined || body.number === undefined) {
-      return response.status(400).json({error: 'content missing'})
-    }
-
-    if (persons.find(p => p.name === body.name)) {
-        return response.status(400).json({error: 'name must be unique'})
-    }
-  
-    const person = {
-      name: body.name,
-      number: body.number,
-      id: Math.floor(Math.random()* 1000000)
-    }
-  
-    persons = persons.concat(person)
-  
+  if (person) {
     response.json(person)
-  })
+  } else {
+    response.status(404).end()
+  }
+})
 
-  const PORT = 3001
-  app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
-  })
+app.delete('/api/persons/:id', (request, response) => {
+  const id = Number(request.params.id)
+  persons = persons.filter(p => p.id !== id)
+
+  response.status(204).end()
+})
+
+app.post('/api/persons/', (request, response) => {
+  const body = request.body
+
+  if (body.name === undefined || body.number === undefined) {
+    return response.status(400).json({ error: 'content missing' })
+  }
+
+  if (persons.find(p => p.name === body.name)) {
+    return response.status(400).json({ error: 'name must be unique' })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 1000000)
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
